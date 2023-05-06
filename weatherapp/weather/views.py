2b6f92b864de33ea4ccf3982dc48589c744d711d
Sean Boonzaier <boonzaiersa@gmail.com>
2023-05-06 12:13:27 +0200

    link form input to post request

--- weatherapp/weather/views.py
+++ weatherapp/weather/views.py
@@ -5,21 +5,22 @@ import requests
 # Create your views here.
 def index(request):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=dcce203617d8ab4c5e665be3d371b2a5'
+    context = None
+    
+    if(request.method == 'POST'):
+        
+        city = request.POST.get('searchLocation')
 
-    city = 'Cape Town'
+        city_weather = requests.get(url.format(city)).json()
 
-    city_weather = requests.get(url.format(city)).json()
+        weather = {
+            'city': city,
+            'temperature': str(city_weather['main']['temp'])+' ° C',
+            'description': city_weather['weather'][0]['description'],
+            'icon': city_weather['weather'][0]['icon']
+        }
 
-    print(city_weather)
-
-    weather = {
-        'city': city,
-        'temperature': city_weather['main']['temp'],
-        'description': city_weather['weather'][0]['description'],
-        'icon': city_weather['weather'][0]['icon']
-    }
-
-    context = {'weather': weather}
+        context = {'weather': weather}
 
     return render(request, 'weather/index.html', context) # returns the index.html template
 
