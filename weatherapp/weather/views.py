9db5688ddf752978a09f07ca53f1b48c2bc43f5e
Sean Boonzaier <boonzaiersa@gmail.com>
2023-05-06 11:44:06 +0200

    Add functionality to access openweather api

--- weatherapp/weather/views.py
+++ weatherapp/weather/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
 
 # Create your views here.
-def home(request):
-    return render(request, 'home.html')
+def index(request):
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=dcce203617d8ab4c5e665be3d371b2a5'
+
+    city = 'Cape Town'
+
+    city_weather = requests.get(url.format(city)).json()
+
+    print(city_weather)
+
+    weather = {
+        'city': city,
+        'temperature': city_weather['main']['temp'],
+        'description': city_weather['weather'][0]['description'],
+        'icon': city_weather['weather'][0]['icon']
+    }
+
+    context = {'weather': weather}
+
+    return render(request, 'weather/index.html', context) # returns the index.html template
 
 
 def about(request):
--- weatherapp/weatherapp/urls.py
+++ weatherapp/weatherapp/urls.py
@@ -15,11 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from weather import views as weatherViews
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', weatherViews.home),
-    path('about/', weatherViews.about),
+    path('', include('weather.urls'))
 ]
